852aba04b490fc570408b4555cbe45e1eb18e5b7
mmore500 <mmore500@users.noreply.github.com>
2025-04-25 19:06:48 +0000

    Deploying to binder-2025-04-08-dev-sequencescreen-neutral.ipynb from @ mmore500/multilevel-selection-concept@0367380283da521ea79ffc22b344696fc6bed9c5 🚀

--- pylib/_diff_sequences.py
+++ pylib/_diff_sequences.py
@@ -1,5 +1,6 @@
 import typing
 
+import numpy as np
 import polars as pl
 
 
@@ -8,30 +9,33 @@ def diff_sequences(
     *,
     ancestral_sequence: str,
 ) -> pl.Series:
-    # build initial dataframe
+
+    N = len(sequences)
+    L = len(ancestral_sequence)
+    assert all(len(seq) == L for seq in sequences)
+
+    # join into a flat string and reshape to 2D array
+    flat = "".join(sequences).encode("ascii")
+    assert len(flat) == N * L
+    seq_arr = np.frombuffer(flat, dtype="S1").reshape((N, L))
+
+    anc_flat = ancestral_sequence.encode("ascii")
+    anc_arr = np.frombuffer(anc_flat, dtype="S1")
+
+    # find mismatches
+    mismatches_idxs = np.argwhere(seq_arr != anc_arr)
+    mismatch_row, mismatch_pos = mismatches_idxs[:, 0], mismatches_idxs[:, 1]
+
+    mismatch_chars = seq_arr[mismatch_row, mismatch_pos]
+
+    # prepare df with mismatches as rows
     df = pl.DataFrame(
         {
-            "sequence": sequences,
-            "ancestral_sequence": ancestral_sequence,
+            "index": mismatch_row,
+            "pos": mismatch_pos,
+            "sequence": mismatch_chars,
         },
-    )
-    assert (len(ancestral_sequence) == df["sequence"].str.len_bytes()).all()
-
-    df = df.lazy()
-
-    # explode each charcter from sequence
-    df = (
-        df.with_row_index()
-        .with_columns(
-            ancestral_sequence=pl.col("ancestral_sequence").str.split(""),
-            sequence=pl.col("sequence").str.split(""),
-        )
-        .explode("ancestral_sequence", "sequence")
-        .with_columns(pos=pl.cum_count("index") % len(ancestral_sequence))
-    )
-
-    # only consider mismatches
-    df = df.filter(pl.col("ancestral_sequence") != pl.col("sequence"))
+    ).lazy()
 
     # prepare diff string for each position
     df = df.with_columns(
@@ -44,16 +48,14 @@ def diff_sequences(
     )
 
     # aggregate each index into one comma‐separated diff string
-    df_diffs = df.group_by("index").agg(
-        diff=pl.col("diff").str.join(","),
-    )
+    df = df.group_by("index").agg(diff=pl.col("diff").str.join(","))
 
     # prepare a base index 0..N to get one row per sequence, even if no diffs
     base = pl.select(index=pl.arange(len(sequences), dtype=pl.UInt32)).lazy()
 
     # left‐join the diffs, wrap in braces, fill nulls to get "{}"
     result = (
-        base.join(df_diffs, how="left", on="index")
+        base.join(df, how="left", on="index")
         .sort("index")
         .select(
             pl.concat_str(
