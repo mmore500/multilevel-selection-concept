39f9ca8f76294eca159c0c43ff9689a236d98482
mmore500 <mmore500.login@gmail.com>
2025-04-25 20:58:26 +0000

    Deploying to binder-2025-04-11-dev-sequenceconfig.ipynb from @ mmore500/multilevel-selection-concept@cd91a3ef24ebdcac050d2440c81768ad300d5439 🚀

--- tests/test_diff_sequences.py
+++ tests/test_diff_sequences.py
@@ -3,8 +3,8 @@ from pylib._diff_sequences import diff_sequences
 
 def test_diff_sequences():
     """Test the diff_sequences function with a simple example."""
-    sequences = ["ACGT", "ACGT", "ACGT", "XCGT"]
+    sequences = ["ACGT", "ACGT", "ACGT", "XCGX"]
     ancestral_sequence = "ACGT"
     assert diff_sequences(
         sequences, ancestral_sequence=ancestral_sequence
-    ).to_list() == ["{}", "{}", "{}", '{0: "X"}']
+    ).to_list() == ["{}", "{}", "{}", '{0: "X", 3: "X"}']
