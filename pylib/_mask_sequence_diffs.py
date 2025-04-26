0815a119e0ee91b07d600bce8cf1ac9140c2791d
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2025-04-26 15:44:34 -0400

    Optimization: prescreen mutation sites

--- pylib/_mask_sequence_diffs.py
+++ pylib/_mask_sequence_diffs.py
@@ -1,3 +1,4 @@
+import io
 import typing
 
 import numpy as np
@@ -9,11 +10,26 @@ def mask_sequence_diffs(
     ancestral_sequence: str,
     sequence_diffs: typing.Sequence[str],
 ) -> typing.Iterable[typing.Tuple[typing.Tuple[int, str], np.ndarray]]:
-    diffs = pl.Series(sequence_diffs).cast(pl.Utf8)
-    for pos in range(len(ancestral_sequence)):
-        vals = diffs.str.json_path_match(f"$.{pos}")
-        if vals.count() == 0:
-            continue
+    diffs = pl.DataFrame(
+        {"diffs": sequence_diffs},
+        schema={"diffs": pl.Utf8},
+    )
+
+    key_blob = (
+        diffs.lazy()
+        .select(pl.col("diffs").str.head(-1).str.tail(-1).str.join(","))
+        .collect()
+        .item()
+    )
+    key_list = " ".join(key_blob.replace(":", ",").split(",")[::2]).replace(
+        '"', ""
+    )
+    keys = np.unique(np.loadtxt(io.StringIO(key_list), dtype=int))
+    keys.sort()
+
+    for pos in keys:
+        vals = diffs["diffs"].str.json_path_match(f"$.{pos}")
+        assert vals.count()
         for char in sorted(vals.unique()):
             assert char != ancestral_sequence[pos]
             yield (pos, char), (vals == char).to_numpy()
