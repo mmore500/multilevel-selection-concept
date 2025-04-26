071ab576b0d9a1bde02497df03b1265b39d8e8a1
mmore500 <mmore500.login@gmail.com>
2025-04-26 18:07:32 +0000

    Deploying to binder-2025-03-31-dev-variantsync.ipynb from @ mmore500/multilevel-selection-concept@533eb1041da26fc44f8ade0c78a2748e2dba1eb1 🚀

--- tests/test_diff_sequences.py
+++ tests/test_diff_sequences.py
@@ -6,5 +6,12 @@ def test_diff_sequences():
     sequences = ["ACGT", "ACGT", "ACGT", "XCGX"]
     ancestral_sequence = "ACGT"
     assert diff_sequences(
-        sequences, ancestral_sequence=ancestral_sequence
+        sequences,
+        ancestral_sequence=ancestral_sequence,
+        quote_keys=False,
     ).to_list() == ["{}", "{}", "{}", '{0: "X", 3: "X"}']
+    assert diff_sequences(
+        sequences,
+        ancestral_sequence=ancestral_sequence,
+        quote_keys=True,
+    ).to_list() == ["{}", "{}", "{}", '{"0": "X", "3": "X"}']
