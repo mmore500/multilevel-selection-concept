2022fc719b71f88e912dfea26950e464deb6bf01
mmore500 <mmore500.login@gmail.com>
2025-04-28 04:55:22 +0000

    Deploying to binder-2024-10-20-example.ipynb from @ mmore500/multilevel-selection-concept@a041797744bdd6f4703440b3b308d39aaa40e77a 🚀

--- slurm/2025-04-25-covaphastsim/2025-04-25-volzdev.sh
+++ slurm/2025-04-25-covaphastsim/2025-04-25-volzdev.sh
@@ -296,7 +296,7 @@ ls -1 "${BATCHDIR}"/__*/**/a=run_volzscreen+* \
     | tee /dev/stderr \
     | python3.10 -m joinem --progress \
         --eager-write --eager-read \
-        --how "diagonal_relaxed" \
+        --how "vertical_relaxed" \
         "${BATCHDIR_JOBRESULT}/a=result+date=${JOBDATE}+job=${JOBNAME}+ext=.pqt"
 ls -l "${BATCHDIR_JOBRESULT}"
 du -h "${BATCHDIR_JOBRESULT}"
