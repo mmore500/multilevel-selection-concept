130d8cb2df111d454a95f46c1071c2bb0709e1ca
mmore500 <mmore500.login@gmail.com>
2025-04-28 05:01:19 +0000

    Deploying to binder-2025-04-08-dev-sequencescreen-neutral.ipynb from @ mmore500/multilevel-selection-concept@a16154a3524421fcdf4c8afa80f0b6e9fda37253 🚀

--- slurm/2025-04-25-covaphastsim/2025-04-25-volzdev.sh
+++ slurm/2025-04-25-covaphastsim/2025-04-25-volzdev.sh
@@ -296,7 +296,7 @@ ls -1 "${BATCHDIR}"/__*/**/a=run_volzscreen+* \
     | tee /dev/stderr \
     | python3.10 -m joinem --progress \
         --eager-write --eager-read \
-        --how "vertical_relaxed" \
+        --how "diagonal_relaxed" \
         "${BATCHDIR_JOBRESULT}/a=result+date=${JOBDATE}+job=${JOBNAME}+ext=.pqt"
 ls -l "${BATCHDIR_JOBRESULT}"
 du -h "${BATCHDIR_JOBRESULT}"
