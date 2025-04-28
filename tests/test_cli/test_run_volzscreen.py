ca218bb79897c40769e68ba6fd1b4ad84d3850dc
mmore500 <mmore500@users.noreply.github.com>
2025-04-28 02:23:28 +0000

    Deploying to binder-2025-04-11-dev-sequenceconfig.ipynb from @ mmore500/multilevel-selection-concept@fbfe4619913e91814430619d4505d5c99a263e8e 🚀

--- tests/test_cli/test_run_volzscreen.py
+++ tests/test_cli/test_run_volzscreen.py
@@ -1,25 +1,79 @@
+import glob
 import os
 import subprocess
+import sys
 
+import more_itertools as mit
 import pytest
 
+from pylib._cd_tmpdir_context import cd_tempdir_context
+
 assets = os.path.join(os.path.dirname(os.path.abspath(__file__)), "../assets")
+pwd = os.getcwd()
 
 
+@cd_tempdir_context()
 @pytest.mark.parametrize("hsurf_bits", [0, 64])
 def test_run_volzscreen_smoke(hsurf_bits: int):
+
     subprocess.run(
         [
-            "python3",
+            sys.executable,
             "-m",
             "pylib.cli.run_volzscreen",
         ],
         check=True,
+        env={"PYTHONPATH": f"{pwd}"},
         input=f"""
-cfg_refphylos: "{assets}/../assets/a=run_covaphastsim.pqt"
+cfg_refphylos: "{assets}/a=run_covaphastsim.pqt"
 screen_num: 0
 trt_hsurf_bits: {hsurf_bits}
-trt_n_downsample: 10000
+trt_n_downsample: 1000
+trt_clade_size_thresh: 4
+""".encode(),
+    )
+
+
+@cd_tempdir_context()
+@pytest.mark.parametrize("hsurf_bits", [0, 64])
+def test_run_volzscreen_covaphastsim_smoke(hsurf_bits: int):
+
+    subprocess.run(
+        [
+            sys.executable,
+            "-m",
+            "pylib.cli.run_covaphastsim",
+        ],
+        check=True,
+        env={"PYTHONPATH": f"{pwd}"},
+        input=f"""
+cfg_p_wt_to_mut: 0.01
+cfg_pop_size: 4000
+cfg_refseqs: "{assets}/alignedsequences.csv"
+cfg_suffix_mut: "'"
+cfg_suffix_wt: "+"
+replicate_num: 0
+trt_mutmx_active_strain_factor: 1.0
+trt_mutmx_rel_beta: 1.0
+trt_mutmx_withinhost_r: 1.0
+trt_name: "test"
+trt_seed: 1
+""".encode(),
+    )
+
+    subprocess.run(
+        [
+            sys.executable,
+            "-m",
+            "pylib.cli.run_volzscreen",
+        ],
+        check=True,
+        env={"PYTHONPATH": pwd},
+        input=f"""
+cfg_refphylos: "{mit.one(glob.glob('*.pqt'))}"
+screen_num: 0
+trt_hsurf_bits: {hsurf_bits}
+trt_n_downsample: 1000
 trt_clade_size_thresh: 4
 """.encode(),
     )
