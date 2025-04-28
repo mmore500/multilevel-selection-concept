fc1f45fce9d9501de1cdc99e1eaddbb4bc520ec0
mmore500 <mmore500@users.noreply.github.com>
2025-04-28 01:22:00 +0000

    Deploying to binder-2025-04-08-dev-sequencescreen-pessimal.ipynb from @ mmore500/multilevel-selection-concept@4112b9623c5c76bc71bd4c4b23a3dabae33a8f3f 🚀

--- pylib/_mask_sequence_diffs.py
+++ pylib/_mask_sequence_diffs.py
@@ -9,6 +9,7 @@ def mask_sequence_diffs(
     *,
     ancestral_sequence: str,
     sequence_diffs: typing.Sequence[str],
+    mut_freq_thresh: int = 0,
     progress_wrap: typing.Callable = lambda x: x,
 ) -> typing.Iterable[typing.Tuple[typing.Tuple[int, str, str], np.ndarray]]:
     diffs = pl.DataFrame(
@@ -18,22 +19,32 @@ def mask_sequence_diffs(
 
     key_blob = (
         diffs.lazy()
-        .select(pl.col("diffs").str.head(-1).str.tail(-1).str.join(","))
+        .select(
+            pl.col("diffs")
+            .filter(pl.col("diffs") != "{}")
+            .str.head(-1)
+            .str.tail(-1)
+            .str.join(",")
+        )
         .collect()
         .item()
     )
     key_list = " ".join(key_blob.replace(":", ",").split(",")[::2]).replace(
         '"', ""
     )
+
     if key_list.strip():
-        keys = np.unique(np.loadtxt(io.StringIO(key_list), dtype=int))
-    else:
-        keys = np.array([], dtype=int)  # avoid numpy warning
+        mut_counts = np.unique_counts(
+            np.loadtxt(io.StringIO(key_list), dtype=int)
+        )
+    else:  # avoid numpy warning
+        mut_counts = np.unique_counts(np.array([], dtype=int))
 
-    keys.sort()
+    frequent_muts = mut_counts.values[mut_counts.counts >= mut_freq_thresh]
+    frequent_muts.sort()
 
-    for pos in progress_wrap(keys):
-        vals = diffs["diffs"].str.json_path_match(f"$.{pos}")
+    for pos in progress_wrap(frequent_muts):
+        vals = diffs["diffs"].str.json_path_match(f"$.{pos}").fill_null(False)
         assert vals.count()
         for char in sorted(vals.unique()):
             assert char != ancestral_sequence[pos]
