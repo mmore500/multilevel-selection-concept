205a56e5fddeb6b6278f97dc6287459b2fe62f4b
mmore500 <mmore500.login@gmail.com>
2025-04-29 23:31:30 +0000

    Deploying to binder-2025-04-07-dev-variantscreen.ipynb from @ mmore500/multilevel-selection-concept@4962b894083cba3f51ec253b0aaa56213f2f961a 🚀

--- slurm_/2025-04-28-covaphast-e2e/2025-04-28-run_covaphastsim.sh
+++ slurm_/2025-04-28-covaphast-e2e/2025-04-28-run_covaphastsim.sh
@@ -215,7 +215,7 @@ echo "cpuinfo ----------------------------------------------------- \${SECONDS}"
 cat /proc/cpuinfo || :
 
 echo "do work ----------------------------------------------------- \${SECONDS}"
-python3 << EOF_ | singularity exec docker://ghcr.io/mmore500/multilevel-selection-concept@sha256:d87764f953ec1d1bc2397d2b696d61994f7800018e5dc082123a295309729bf1 python3 -m pylib.cli.run_covaphastsim
+python3 << EOF_ | singularity exec docker://ghcr.io/mmore500/multilevel-selection-concept@sha256:9f127141271ae4e1abce2daec571a25052062e182ca04bf9bdd0d01154f19060 python3 -m pylib.cli.run_covaphastsim
 
 import itertools as it
 import os
