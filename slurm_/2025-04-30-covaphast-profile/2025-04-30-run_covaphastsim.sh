7e5c839299b03c157f10faa99a8b77859275c4df
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2025-05-01 19:50:46 -0400

    Scale up profiling sim

--- slurm_/2025-04-30-covaphast-profile/2025-04-30-run_covaphastsim.sh
+++ slurm_/2025-04-30-covaphast-profile/2025-04-30-run_covaphastsim.sh
@@ -234,9 +234,8 @@ trt_mutmx_withinhost_r = {"Sdel": None, "Sneu": 1.0, "Sben": 2.0}[S]
 
 cfg = f"""
 cfg_num_mut_sites: 1
-# cfg_p_wt_to_mut: {2.74e-6 / 3}  # divide by 3 possible alt nucleotides
-cfg_p_wt_to_mut: {2.74e-5}  # divide by 3 possible alt nucleotides
-cfg_pop_size: {67_000_000 // 500}
+cfg_p_wt_to_mut: {2.74e-6}  # should divide by 3 possible alt nucleotides?
+cfg_pop_size: {67_000_000 // 100}
 cfg_maxseqlen: 390
 cfg_refseqs: "https://osf.io/s9xhr/download"  # homogenized seqs for testing
 cfg_suffix_mut: "'"
