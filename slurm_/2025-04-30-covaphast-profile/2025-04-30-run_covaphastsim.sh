8c422c786a83c021ea8bc381888f42d079ac084a
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2025-05-01 19:47:45 -0400

    Adjust computational resources

--- slurm_/2025-04-30-covaphast-profile/2025-04-30-run_covaphastsim.sh
+++ slurm_/2025-04-30-covaphast-profile/2025-04-30-run_covaphastsim.sh
@@ -193,8 +193,8 @@ echo "SBATCH_FILE ${SBATCH_FILE}"
 cat > "${SBATCH_FILE}" << EOF
 #!/bin/bash
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=4
-#SBATCH --mem=100G
+#SBATCH --cpus-per-task=2
+#SBATCH --mem=50G
 #SBATCH --time=4:00:00
 #SBATCH --output="/mnt/home/%u/joblog/%j"
 #SBATCH --mail-user=mawni4ah2o@pomail.net
