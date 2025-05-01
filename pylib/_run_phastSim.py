f411937b757d5a6d492efa74e8568ce60e8f373c
mmore500 <mmore500.login@gmail.com>
2025-05-01 17:43:15 +0000

    Deploying to binder-2025-04-11-dev-sequenceconfig.ipynb from @ mmore500/multilevel-selection-concept@3a84c46555b20412d3c7c4fbd0cde619b3b57c24 🚀

--- pylib/_run_phastSim.py
+++ pylib/_run_phastSim.py
@@ -103,6 +103,7 @@ def _do_run_phastSim(
     # sim_run.const.alleles
     with hstrat_aux.log_context_duration("phastSimRun", print):
         sim_run = phastSim.phastSimRun(args=args)
+    # np.random.seed(args.seed)
     hierarchy = not args.noHierarchy
 
     # initialise the root genome. Reads either from file or creates a genome in
