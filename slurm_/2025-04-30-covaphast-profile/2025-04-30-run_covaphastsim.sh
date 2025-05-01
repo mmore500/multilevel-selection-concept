4f1dd9b2d9bc08b122a210429066fc4dd686b442
mmore500 <mmore500.login@gmail.com>
2025-05-01 17:00:53 +0000

    Deploying to binder from @ mmore500/multilevel-selection-concept@66d1548314106073f703fa0725cd85b032c70c48 🚀

--- slurm_/2025-04-30-covaphast-profile/2025-04-30-run_covaphastsim.sh
+++ slurm_/2025-04-30-covaphast-profile/2025-04-30-run_covaphastsim.sh
@@ -229,13 +229,12 @@ replicates = it.product(
 )
 
 trt_mutmx_active_strain_factor = {"Gdel": 0.5, "Gneu": 1.0, "Gben": None}[G]
-trt_mutmx_rel_beta = {"Gdel": 0.5, "Gneu": 1.0, "Gben": None}[G]
+trt_mutmx_rel_beta = {"Gdel": 1.0, "Gneu": 1.0, "Gben": None}[G]
 trt_mutmx_withinhost_r = {"Sdel": None, "Sneu": 1.0, "Sben": 2.0}[S]
 
 cfg = f"""
 cfg_num_mut_sites: 1
-# cfg_p_wt_to_mut: 2.74e-6 / 3  # divide by 3 possible alt nucleotides
-cfg_p_wt_to_mut: 2.74e-5
+cfg_p_wt_to_mut: 2.74e-6 / 3  # divide by 3 possible alt nucleotides
 cfg_pop_size: {67_000_000 // 100}
 cfg_maxseqlen: 390
 cfg_refseqs: "https://osf.io/7s9z8/download"
