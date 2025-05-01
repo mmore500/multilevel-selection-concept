66d1548314106073f703fa0725cd85b032c70c48
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2025-05-01 10:46:29 -0400

    Restore match to base rate

--- slurm_/2025-04-30-covaphast-profile/2025-04-30-run_covaphastsim.sh
+++ slurm_/2025-04-30-covaphast-profile/2025-04-30-run_covaphastsim.sh
@@ -234,8 +234,7 @@ trt_mutmx_withinhost_r = {"Sdel": None, "Sneu": 1.0, "Sben": 2.0}[S]
 
 cfg = f"""
 cfg_num_mut_sites: 1
-# cfg_p_wt_to_mut: 2.74e-6 / 3  # divide by 3 possible alt nucleotides
-cfg_p_wt_to_mut: 2.74e-5
+cfg_p_wt_to_mut: 2.74e-6 / 3  # divide by 3 possible alt nucleotides
 cfg_pop_size: {67_000_000 // 100}
 cfg_maxseqlen: 390
 cfg_refseqs: "https://osf.io/7s9z8/download"
