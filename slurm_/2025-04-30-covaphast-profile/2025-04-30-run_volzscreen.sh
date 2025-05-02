aab3c3f3f58a6eee772deb1149eb1953e56ecf58
mmore500 <mmore500.login@gmail.com>
2025-05-02 21:47:18 +0000

    Deploying to binder-2025-04-30-profile-mut-freqs.ipynb from @ mmore500/multilevel-selection-concept@92198e752e5dce8260d271ae3a8bb448e7d8c89d 🚀

--- slurm_/2025-04-30-covaphast-profile/2025-04-30-run_volzscreen.sh
+++ slurm_/2025-04-30-covaphast-profile/2025-04-30-run_volzscreen.sh
@@ -19,7 +19,7 @@ echo "SOURCE_REVISION ${SOURCE_REVISION}"
 SOURCE_REMOTE_URL="$(git config --get remote.origin.url)"
 echo "SOURCE_REMOTE_URL ${SOURCE_REMOTE_URL}"
 
-CONTAINER_URI="docker://ghcr.io/mmore500/multilevel-selection-concept@sha256:2f5ba7c846420ce347a39d220531bb1a7b2f7048e64975445d51a8a75ad8c003"
+CONTAINER_URI="docker://ghcr.io/mmore500/multilevel-selection-concept@sha256:423cd888e037c8df47439bed57fca13ef691cd9332ff645f0a1839ed976a26a8"
 echo "CONTAINER_URI ${CONTAINER_URI}"
 
 echo "initialization telemetry ==============================================="
