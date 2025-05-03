68c78e094334df42510b07803953c445925e0b54
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2025-05-03 18:43:31 -0400

    Add 50% naive screens

--- pylib/_screen_mutation_defined_nodes.py
+++ pylib/_screen_mutation_defined_nodes.py
@@ -27,23 +27,38 @@ def screen_mutation_defined_nodes(
         phylo_df["ancestor_id"].to_numpy()
     ] &= ~screen_trait_defined_fisher
 
-    screen_trait_defined_naive = (
+    screen_trait_defined_naive50 = (
         hstrat_aux.alifestd_screen_trait_defined_clades_naive_asexual(
             phylo_df,
             mutate=True,
             mask_trait_absent=(~has_mutation) & phylo_df["is_leaf"],
             mask_trait_present=has_mutation & phylo_df["is_leaf"],
+            defining_mut_thresh=0.50,
+            defining_mut_sister_thresh=0.50,
+        )
+    )
+    screen_trait_defined_naive75 = (
+        hstrat_aux.alifestd_screen_trait_defined_clades_naive_asexual(
+            phylo_df,
+            mutate=True,
+            mask_trait_absent=(~has_mutation) & phylo_df["is_leaf"],
+            mask_trait_present=has_mutation & phylo_df["is_leaf"],
+            defining_mut_thresh=0.75,
+            defining_mut_sister_thresh=0.75,
         )
     )
 
-    combined = screen_trait_defined_fisher & screen_trait_defined_naive
     fisher = screen_trait_defined_fisher
-    naive = screen_trait_defined_naive
+    naive50 = screen_trait_defined_naive50
+    naive75 = screen_trait_defined_naive75
+    combined50 = fisher & naive50
+    combined75 = fisher & naive75
     return {
-        "combined": combined,
+        "combined50": combined50,
+        "combined75": combined75,
+        "naive50": naive50,
+        "naive75": naive75,
         "fisher": fisher,
-        "naive": naive,
-        "ctrl_combined": np.random.permutation(combined.copy()),
         "ctrl_fisher": np.random.permutation(fisher.copy()),
-        "ctrl_naive": np.random.permutation(naive.copy()),
+        "ctrl_naive75": np.random.permutation(naive75.copy()),
     }
