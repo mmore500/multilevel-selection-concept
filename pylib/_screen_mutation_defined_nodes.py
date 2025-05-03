c0501246936205303305a968334137fdbe05533d
mmore500 <mmore500@users.noreply.github.com>
2025-05-03 23:14:52 +0000

    Deploying to binder-2025-04-08-dev-sequencescreen-pessimal.ipynb from @ mmore500/multilevel-selection-concept@414f8fe6af15ea01003cfeae990da2c5baa7e2c0 🚀

--- pylib/_screen_mutation_defined_nodes.py
+++ pylib/_screen_mutation_defined_nodes.py
@@ -1,3 +1,5 @@
+import typing
+
 from hstrat import _auxiliary_lib as hstrat_aux
 import numpy as np
 import pandas as pd
@@ -6,11 +8,12 @@ import pandas as pd
 def screen_mutation_defined_nodes(
     phylo_df: pd.DataFrame,
     has_mutation: pd.Series,
+    screens: typing.Sequence[str] = ("combined", "naive", "fisher"),
 ) -> dict:
     phylo_df.reset_index(drop=True, inplace=True)
 
     # trait screening --- trait-defined
-    screen_trait_defined_fisher = (
+    screen_trait_defined_fisher20 = (
         hstrat_aux.alifestd_screen_trait_defined_clades_fisher_asexual(
             phylo_df,
             mutate=True,
@@ -23,9 +26,9 @@ def screen_mutation_defined_nodes(
     # i..e, if there is a pocket of trait-having leaves, several inner nodes
     # above may test as significant, but we want the furthest down
     assert hstrat_aux.alifestd_is_working_format_asexual(phylo_df)
-    screen_trait_defined_fisher[
+    screen_trait_defined_fisher20[
         phylo_df["ancestor_id"].to_numpy()
-    ] &= ~screen_trait_defined_fisher
+    ] &= ~screen_trait_defined_fisher20
 
     screen_trait_defined_naive50 = (
         hstrat_aux.alifestd_screen_trait_defined_clades_naive_asexual(
@@ -48,17 +51,23 @@ def screen_mutation_defined_nodes(
         )
     )
 
-    fisher = screen_trait_defined_fisher
+    screens = list(screens)
+
+    fisher20 = screen_trait_defined_fisher20
     naive50 = screen_trait_defined_naive50
     naive75 = screen_trait_defined_naive75
-    combined50 = fisher & naive50
-    combined75 = fisher & naive75
-    return {
-        "combined50": combined50,
-        "combined75": combined75,
-        "naive50": naive50,
-        "naive75": naive75,
-        "fisher": fisher,
-        "ctrl_fisher": np.random.permutation(fisher.copy()),
-        "ctrl_naive75": np.random.permutation(naive75.copy()),
+
+    recipes = {
+        "combined": lambda: fisher20 & naive75,
+        "combined_f20n50": lambda: fisher20 & naive50,
+        "combined_f20n75": lambda: fisher20 & naive75,
+        "naive": lambda: naive75,
+        "naive50": lambda: naive50,
+        "naive75": lambda: naive75,
+        "fisher": lambda: fisher20,
+        "fisher20": lambda: fisher20,
+        "ctrl_fisher20": lambda: np.random.permutation(fisher20.copy()),
+        "ctrl_naive75": lambda: np.random.permutation(naive75.copy()),
     }
+
+    return {item: recipes[item]() for item in screens}
