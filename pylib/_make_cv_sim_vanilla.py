eaeb661b3acd21120d3ad2f9931fab73d20e5c1e
mmore500 <mmore500.login@gmail.com>
2025-05-05 00:03:54 +0000

    Deploying to binder-2025-04-11-dev-sequenceconfig.ipynb from @ mmore500/multilevel-selection-concept@8b97b2a1abdf7086be15f91ec2e97db0054133fb 🚀

---
+++ pylib/_make_cv_sim_vanilla.py
@@ -0,0 +1,30 @@
+import typing
+
+import covasim as cv
+
+
+def make_cv_sim_vanilla(
+    *,
+    preinterventions: typing.Sequence[object] = tuple(),
+    postinterventions: typing.Sequence[object] = tuple(),
+    pop_size: int = 100_000,
+    seed: int = 1,
+    variants: typing.List[cv.variant],
+) -> cv.Sim:
+
+    sim = cv.Sim(
+        interventions=[
+            *preinterventions,
+            *postinterventions,
+        ],
+        n_days=200,
+        use_waning=False,
+        pop_infected=0,  # disable wild-type strain
+        pop_size=pop_size,
+        variants=variants,
+        rand_seed=seed,
+    )
+
+    sim.initialize()
+
+    return sim
