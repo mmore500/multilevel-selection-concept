bb78907176571ef2fdeda842be9427f864e05e21
mmore500 <mmore500.login@gmail.com>
2025-05-05 17:21:35 +0000

    Deploying to binder-2025-04-08-dev-sequencescreen.ipynb from @ mmore500/multilevel-selection-concept@b0da4bfb40a424b86f331680b1c015a904b0e094 🚀

--- pylib/_bootstrap_extrema_quantile.py
+++ pylib/_bootstrap_extrema_quantile.py
@@ -3,24 +3,27 @@ import typing
 import numpy as np
 from scipy import stats as scipy_stats
 
+from ._bootstrap_extrema_values import bootstrap_extrema_values
+
 
 def bootstrap_extrema_quantile(
     data: np.ndarray,
     sample_value: float,
     sample_size: int,
+    weights: typing.Optional[np.ndarray] = None,
     extrema: typing.Callable = np.max,
     n_bootstrap: int = 1000,
 ) -> float:
     """Calculate the quantile of a sample value in a bootstrap distribution of
     extrema values."""
 
-    # Convert data to a numpy array
-    data = np.asarray(data)
-
-    samples = np.random.choice(
-        data, size=(n_bootstrap, sample_size), replace=True
+    extrema_values = bootstrap_extrema_values(
+        data,
+        sample_size=sample_size,
+        weights=weights,
+        extrema=extrema,
+        n_bootstrap=n_bootstrap,
     )
-    extrema_values = np.apply_along_axis(extrema, 1, samples)
     return (
         scipy_stats.percentileofscore(
             extrema_values, sample_value, kind="rank"
