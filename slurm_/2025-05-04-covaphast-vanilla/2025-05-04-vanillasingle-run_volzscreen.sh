280581449201e24b784f2a3c44418b8012cacc38
mmore500 <mmore500.login@gmail.com>
2025-05-06 20:46:49 +0000

    Deploying to binder from @ mmore500/multilevel-selection-concept@b6986c3cf237169944fee39cdd1f7ee108836e3d 🚀

--- slurm_/2025-05-04-covaphast-vanilla/2025-05-04-vanillasingle-run_volzscreen.sh
+++ slurm_/2025-05-04-covaphast-vanilla/2025-05-04-vanillasingle-run_volzscreen.sh
@@ -19,7 +19,7 @@ echo "SOURCE_REVISION ${SOURCE_REVISION}"
 SOURCE_REMOTE_URL="$(git config --get remote.origin.url)"
 echo "SOURCE_REMOTE_URL ${SOURCE_REMOTE_URL}"
 
-CONTAINER_URI="docker://ghcr.io/mmore500/multilevel-selection-concept@sha256:93d519dba50acd8f4f2c43c4543d8c124aeaff48db9fa19b0ad4ffabb9bc134e"
+CONTAINER_URI="docker://ghcr.io/mmore500/multilevel-selection-concept@sha256:4e595b781be9313dcdca44862d6a152a995fa70356f1e1eb3646bc295d54a392"
 echo "CONTAINER_URI ${CONTAINER_URI}"
 
 echo "initialization telemetry ==============================================="
---
+++ slurm_/2025-05-05-covaphast-vanillasimple/2025-05-05-vanillasimplesingle-run_volzscreen.sh
@@ -0,0 +1,386 @@
+#!/bin/bash
+
+set -e
+
+cd "$(dirname "$0")"
+
+echo "configuration ==========================================================="
+JOBDATE="$(date '+%Y-%m-%d')"
+echo "JOBDATE ${JOBDATE}"
+
+JOBNAME="$(basename -s .sh "$0")"
+echo "JOBNAME ${JOBNAME}"
+
+JOBPROJECT="$(basename -s .git "$(git remote get-url origin)")"
+echo "JOBPROJECT ${JOBPROJECT}"
+
+SOURCE_REVISION="$(git rev-parse HEAD)"
+echo "SOURCE_REVISION ${SOURCE_REVISION}"
+SOURCE_REMOTE_URL="$(git config --get remote.origin.url)"
+echo "SOURCE_REMOTE_URL ${SOURCE_REMOTE_URL}"
+
+CONTAINER_URI="docker://ghcr.io/mmore500/multilevel-selection-concept@sha256:0cc6b1a7439407955b44cf336b13f35a457fe10785f67da76434a9e00d6560de"
+echo "CONTAINER_URI ${CONTAINER_URI}"
+
+echo "initialization telemetry ==============================================="
+echo "date $(date)"
+echo "hostname $(hostname)"
+echo "PWD ${PWD}"
+echo "SLURM_JOB_ID ${SLURM_JOB_ID:-nojid}"
+echo "SLURM_ARRAY_TASK_ID ${SLURM_ARRAY_TASK_ID:-notid}"
+module purge || :
+module load Python/3.10.8 || :
+echo "python3.10 $(which python3.10)"
+echo "python3.10 --version $(python3.10 --version)"
+echo "singularity $(which singularity)"
+echo "singularity --version $(singularity --version)"
+
+echo "setup HOME dirs ========================================================"
+mkdir -p "${HOME}/joblatest"
+mkdir -p "${HOME}/joblog"
+mkdir -p "${HOME}/jobscript"
+if ! [ -e "${HOME}/scratch" ]; then
+    if [ -e "/mnt/scratch/${USER}" ]; then
+        ln -s "/mnt/scratch/${USER}" "${HOME}/scratch" || :
+    else
+        mkdir -p "${HOME}/scratch" || :
+    fi
+fi
+
+echo "setup BATCHDIR =========================================================="
+BATCHDIR="${HOME}/scratch/${JOBPROJECT}/${JOBNAME}/${JOBDATE}"
+if [ -e "${BATCHDIR}" ]; then
+    echo "BATCHDIR ${BATCHDIR} exists, clearing it"
+fi
+rm -rf "${BATCHDIR}"
+mkdir -p "${BATCHDIR}"
+echo "BATCHDIR ${BATCHDIR}"
+
+echo "symlinking latest"
+LATESTDIR="${HOME}/scratch/${JOBPROJECT}/${JOBNAME}/latest"
+echo "${BATCHDIR} > ${LATESTDIR}"
+ln -sfn "${BATCHDIR}" "${LATESTDIR}"
+
+BATCHDIR_JOBLOG="${BATCHDIR}/joblog"
+echo "BATCHDIR_JOBLOG ${BATCHDIR_JOBLOG}"
+mkdir -p "${BATCHDIR_JOBLOG}"
+
+BATCHDIR_JOBRESULT="${BATCHDIR}/jobresult"
+echo "BATCHDIR_JOBRESULT ${BATCHDIR_JOBRESULT}"
+mkdir -p "${BATCHDIR_JOBRESULT}"
+
+BATCHDIR_JOBSCRIPT="${BATCHDIR}/jobscript"
+echo "BATCHDIR_JOBSCRIPT ${BATCHDIR_JOBSCRIPT}"
+mkdir -p "${BATCHDIR_JOBSCRIPT}"
+
+BATCHDIR_JOBSOURCE="${BATCHDIR}/_jobsource"
+echo "BATCHDIR_JOBSOURCE ${BATCHDIR_JOBSOURCE}"
+if [[ $* == *--dirty* ]]; then
+    cp -r "$(git rev-parse --show-toplevel)" "${BATCHDIR_JOBSOURCE}"
+else
+    mkdir -p "${BATCHDIR_JOBSOURCE}"
+    for attempt in {1..5}; do
+        rm -rf "${BATCHDIR_JOBSOURCE}/.git"
+        git -C "${BATCHDIR_JOBSOURCE}" init \
+        && git -C "${BATCHDIR_JOBSOURCE}" remote add origin "${SOURCE_REMOTE_URL}" \
+        && git -C "${BATCHDIR_JOBSOURCE}" fetch origin "${SOURCE_REVISION}" --depth=1 \
+        && git -C "${BATCHDIR_JOBSOURCE}" reset --hard FETCH_HEAD \
+        && break || echo "failed to clone, retrying..."
+        if [ $attempt -eq 5 ]; then
+            echo "failed to clone, failing"
+            exit 1
+        fi
+        sleep 5
+    done
+fi
+
+BATCHDIR_ENV="${BATCHDIR}/_jobenv"
+python3.10 -m venv --system-site-packages "${BATCHDIR_ENV}"
+source "${BATCHDIR_ENV}/bin/activate"
+echo "python3.10 $(which python3.10)"
+echo "python3.10 --version $(python3.10 --version)"
+for attempt in {1..5}; do
+    python3.10 -m pip install --upgrade pip setuptools wheel || :
+    python3.10 -m pip install --upgrade uv \
+    && python3.10 -m uv pip install \
+        'more_itertools==10.*' \
+        'numpy==2.*' \
+        'joinem==0.9.3' \
+        'pandas==2.*' \
+        'polars==1.29.*' \
+        'polars-u64-idx==1.29.*' \
+        'pyarrow==16.*' \
+        'scipy==1.*' \
+        'tqdm==4.*' \
+        "${BATCHDIR_JOBSOURCE}" \
+    && break || echo "pip install attempt ${attempt} failed"
+    if [ ${attempt} -eq 3 ]; then
+        echo "pip install failed"
+        exit 1
+    fi
+done
+
+echo "setup dependencies ========================================== \${SECONDS}"
+source "${BATCHDIR_ENV}/bin/activate"
+python3.10 -m uv pip freeze
+
+# prime singularity cache
+singularity exec "${CONTAINER_URI}" echo "hello from singularity"
+
+echo "sbatch preamble ========================================================="
+JOB_PREAMBLE=$(cat << EOF
+set -e
+shopt -s globstar
+
+# adapted from https://unix.stackexchange.com/a/504829
+handlefail() {
+    echo ">>>error<<<" || :
+    awk 'NR>L-4 && NR<L+4 { printf "%-5d%3s%s\n",NR,(NR==L?">>>":""),\$0 }' L=\$1 \$0 || :
+    ln -sfn "\${JOBSCRIPT}" "\${HOME}/joblatest/jobscript.failed" || :
+    ln -sfn "\${JOBLOG}" "\${HOME}/joblatest/joblog.failed" || :
+    $(which scontrol || which echo) requeuehold "${SLURM_JOBID:-nojid}"
+}
+trap 'handlefail $LINENO' ERR
+
+echo "initialization telemetry ------------------------------------ \${SECONDS}"
+echo "SOURCE_REVISION ${SOURCE_REVISION}"
+echo "BATCHDIR ${BATCHDIR}"
+
+echo "cc SLURM script --------------------------------------------- \${SECONDS}"
+JOBSCRIPT="\${HOME}/jobscript/\${SLURM_JOB_ID:-nojid}"
+echo "JOBSCRIPT \${JOBSCRIPT}"
+cp "\${0}" "\${JOBSCRIPT}"
+chmod +x "\${JOBSCRIPT}"
+cp "\${JOBSCRIPT}" "${BATCHDIR_JOBSCRIPT}/\${SLURM_JOB_ID:-nojid}"
+ln -sfn "\${JOBSCRIPT}" "${HOME}/joblatest/jobscript.launched"
+
+echo "cc job log -------------------------------------------------- \${SECONDS}"
+JOBLOG="\${HOME}/joblog/\${SLURM_JOB_ID:-nojid}"
+echo "JOBLOG \${JOBLOG}"
+touch "\${JOBLOG}"
+ln -sfn "\${JOBLOG}" "${BATCHDIR_JOBLOG}/\${SLURM_JOB_ID:-nojid}"
+ln -sfn "\${JOBLOG}" "\${HOME}/joblatest/joblog.launched"
+
+echo "setup JOBDIR ------------------------------------------------ \${SECONDS}"
+JOBDIR="${BATCHDIR}/__\${SLURM_ARRAY_TASK_ID:-\${SLURM_JOB_ID:-\${RANDOM}}}"
+echo "JOBDIR \${JOBDIR}"
+if [ -e "\${JOBDIR}" ]; then
+    echo "JOBDIR \${JOBDIR} exists, clearing it"
+fi
+rm -rf "\${JOBDIR}"
+mkdir -p "\${JOBDIR}"
+cd "\${JOBDIR}"
+echo "PWD \${PWD}"
+
+echo "job telemetry ----------------------------------------------- \${SECONDS}"
+echo "source SLURM_JOB_ID ${SLURM_JOB_ID:-nojid}"
+echo "current SLURM_JOB_ID \${SLURM_JOB_ID:-nojid}"
+echo "SLURM_ARRAY_TASK_ID \${SLURM_ARRAY_TASK_ID:-notid}"
+echo "hostname \$(hostname)"
+echo "date \$(date)"
+
+echo "module setup ------------------------------------------------ \${SECONDS}"
+module purge || :
+module load Python/3.10.8 || :
+echo "python3.10 \$(which python3.10)"
+echo "python3.10 --version \$(python3.10 --version)"
+echo "singularity \$(which singularity)"
+echo "singularity --version \$(singularity --version)"
+
+echo "setup dependencies- ----------------------------------------- \${SECONDS}"
+source "${BATCHDIR_ENV}/bin/activate"
+python3.10 -m uv pip freeze
+
+EOF
+)
+
+echo "create sbatch file: work ==============================================="
+
+SBATCH_FILE="$(mktemp)"
+echo "SBATCH_FILE ${SBATCH_FILE}"
+
+###############################################################################
+# WORK ---------------------------------------------------------------------- #
+###############################################################################
+cat > "${SBATCH_FILE}" << EOF
+#!/bin/bash
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=16
+#SBATCH --mem=50G
+#SBATCH --time=4:00:00
+#SBATCH --output="/mnt/home/%u/joblog/%j"
+#SBATCH --mail-user=mawni4ah2o@pomail.net
+#SBATCH --mail-type=FAIL,TIME_LIMIT
+#SBATCH --account=beacon
+#SBATCH --requeue
+#SBATCH --array=0-29
+
+${JOB_PREAMBLE}
+
+echo "lscpu ------------------------------------------------------- \${SECONDS}"
+lscpu || :
+
+echo "lshw -------------------------------------------------------- \${SECONDS}"
+lshw || :
+
+echo "cpuinfo ----------------------------------------------------- \${SECONDS}"
+cat /proc/cpuinfo || :
+
+echo "do work ----------------------------------------------------- \${SECONDS}"
+python3 << EOF_ | singularity exec "${CONTAINER_URI}" python3 -m pylib.cli.run_volzscreen
+
+import itertools as it
+import logging
+import os
+import sys
+
+import pandas as pd
+from retry import retry
+
+
+refphylos = "https://osf.io/5zv3d/download"
+
+read_parquet = retry(tries=5, logger=logging.getLogger(__name__))(pd.read_parquet)
+uuids = sorted(
+    read_parquet(refphylos)["replicate_uuid"].unique().astype(str),
+)
+
+replicates = it.product(
+    uuids,
+    [0, 16],
+    [1_000_000],
+)
+assigned_uuid, hsurf_bits, ndownsamp = next(
+    it.islice(replicates, \${SLURM_ARRAY_TASK_ID:-0}, None),
+)
+cfg = f"""
+cfg_assigned_replicate_uuid: "{assigned_uuid}"
+cfg_clade_size_thresh: "[0, 3]"
+cfg_mut_count_thresh_lb: 0
+cfg_mut_count_thresh_ub: {sys.maxsize}
+cfg_mut_freq_thresh_lb: 0.0
+cfg_mut_freq_thresh_ub: 1.0
+cfg_mut_quant_thresh_lb: 0.0
+cfg_mut_quant_thresh_ub: 1.0
+cfg_refphylos: "{refphylos}"
+screen_num: \${SLURM_ARRAY_TASK_ID:-0}
+trt_hsurf_bits: {hsurf_bits}
+trt_n_downsample: {ndownsamp}
+trt_seed: \${SLURM_ARRAY_TASK_ID:-0}
+SLURM_JOB_ID: \${SLURM_JOB_ID:-null}
+SLURM_ARRAY_JOB_ID: \${SLURM_ARRAY_JOB_ID:-null}
+SLURM_ARRAY_TASK_ID: \${SLURM_ARRAY_TASK_ID:-null}
+SLURM_ARRAY_TASK_COUNT: \${SLURM_ARRAY_TASK_COUNT:-null}
+SLURM_ARRAY_TASK_MAX: \${SLURM_ARRAY_TASK_MAX:-null}
+SLURM_ARRAY_TASK_MIN: \${SLURM_ARRAY_TASK_MIN:-null}
+"""
+
+print(cfg)
+
+EOF_
+
+echo "finalization telemetry -------------------------------------- \${SECONDS}"
+ls -l \${JOBDIR}
+du -h \${JOBDIR}
+ln -sfn "\${JOBSCRIPT}" "${HOME}/joblatest/jobscript.finished"
+ln -sfn "\${JOBLOG}" "${HOME}/joblatest/joblog.finished"
+echo "SECONDS \${SECONDS}"
+echo '>>>complete<<<'
+
+EOF
+###############################################################################
+# --------------------------------------------------------------------------- #
+###############################################################################
+
+
+echo "submit sbatch file ====================================================="
+$(which sbatch && echo --job-name="${JOBNAME}" || which bash) "${SBATCH_FILE}"
+
+echo "create sbatch file: collate ============================================"
+
+SBATCH_FILE="$(mktemp)"
+echo "SBATCH_FILE ${SBATCH_FILE}"
+
+###############################################################################
+# COLLATE ------------------------------------------------------------------- #
+###############################################################################
+cat > "${SBATCH_FILE}" << EOF
+#!/bin/bash
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=16G
+#SBATCH --time=4:00:00
+#SBATCH --output="/mnt/home/%u/joblog/%j"
+#SBATCH --mail-user=mawni4ah2o@pomail.net
+#SBATCH --mail-type=ALL
+#SBATCH --account=beacon
+#SBATCH --requeue
+
+${JOB_PREAMBLE}
+
+echo "BATCHDIR ${BATCHDIR}"
+ls -l "${BATCHDIR}"
+
+echo "finalize ---------------------------------------------------- \${SECONDS}"
+echo "   - archive job dir"
+pushd "${BATCHDIR}/.."
+    tar czvf \
+    "${BATCHDIR_JOBRESULT}/a=jobarchive+date=${JOBDATE}+job=${JOBNAME}+ext=.tar.gz" \
+    "$(basename "${BATCHDIR}")"/__*
+popd
+
+echo "   - join result"
+ls -1 "${BATCHDIR}"/__*/**/a=run_volzscreen+* \
+    | tee /dev/stderr \
+    | python3.10 -m joinem --progress \
+        --eager-write --eager-read \
+        --how "diagonal_relaxed" \
+        "${BATCHDIR_JOBRESULT}/a=result+date=${JOBDATE}+job=${JOBNAME}+ext=.pqt"
+ls -l "${BATCHDIR_JOBRESULT}"
+du -h "${BATCHDIR_JOBRESULT}"
+
+echo "   - archive joblog"
+pushd "${BATCHDIR}"
+    tar czf \
+    "${BATCHDIR_JOBRESULT}/a=joblog+date=${JOBDATE}+job=${JOBNAME}+ext=.tar.gz" \
+    -h "$(basename "${BATCHDIR_JOBLOG}")"
+popd
+
+echo "   - archive jobscript"
+pushd "${BATCHDIR}"
+    tar czfv \
+    "$(basename "${BATCHDIR_JOBRESULT}")/a=jobscript+date=${JOBDATE}+job=${JOBNAME}+ext=.tar.gz" \
+    -h "$(basename ${BATCHDIR_JOBSCRIPT})"
+popd
+
+ls -l "${BATCHDIR}"
+
+echo "cleanup ----------------------------------------------------- \${SECONDS}"
+cd "${BATCHDIR}"
+for f in _*; do
+    echo "tar and rm \$f"
+    tar cf "\${f}.tar" -h "\${f}"
+    rm -rf "\${f}"
+done
+cd
+ls -l "${BATCHDIR}"
+
+echo "finalization telemetry -------------------------------------- \${SECONDS}"
+ln -sfn "\${JOBSCRIPT}" "\${HOME}/joblatest/jobscript.completed"
+ln -sfn "\${JOBLOG}" "\${HOME}/joblatest/joblog.completed"
+ln -sfn "${BATCHDIR_JOBRESULT}" "\${HOME}/joblatest/jobresult.completed"
+echo "SECONDS \${SECONDS}"
+echo '>>>complete<<<'
+
+EOF
+###############################################################################
+# --------------------------------------------------------------------------- #
+###############################################################################
+
+echo "submit sbatch file ====================================================="
+$(which sbatch && echo --job-name="${JOBNAME}" --dependency=singleton || which bash) "${SBATCH_FILE}"
+
+echo "finalization telemetry ================================================="
+echo "BATCHDIR ${BATCHDIR}"
+echo "SECONDS ${SECONDS}"
+echo '>>>complete<<<'
