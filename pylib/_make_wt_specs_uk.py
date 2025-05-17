db44e238548fa8e6f7cebd67ff009f36fbc733b8
mmore500 <mmore500.login@gmail.com>
2025-05-17 20:03:22 +0000

    Deploying to binder-2025-05-16-vanilla-comparator.ipynb from @ mmore500/multilevel-selection-concept@5a8a139e6f1b7655beb6f93fe505ce69dcabc53b 🚀

---
+++ pylib/_make_wt_specs_uk.py
@@ -0,0 +1,108 @@
+# adapted from
+# https://github.com/Jasminapg/Covid-19-Analysis/blob/7a26db7b5f985e09a4af30e6d6605a570535940b/8_omicron_analysis/calibrate_uk.py
+
+from datetime import date
+import typing
+
+import numpy as np
+
+from ._VariantSpec import VariantSpec
+
+
+def make_wt_specs_uk(
+    reference_sequences: dict,
+    start_day: str = "2020-01-20",
+) -> typing.List[VariantSpec]:
+
+    baseline_variant_params = {
+        "rel_beta": 1.0,
+        "rel_symp_prob": 1.0,
+        "rel_severe_prob": 1.0,
+        "rel_crit_prob": 1.0,
+        "rel_death_prob": 1.0,
+    }
+
+    wildtype = VariantSpec(
+        sequence=reference_sequences["Wildtype"],
+        variant=baseline_variant_params,
+        label="Wildtype",
+        days=[0],
+        n_imports=50,
+    )
+
+    # adding different variants: B.1.177 in September 2020, Alpha slightly
+    # later and Delta from April 2021
+    start_day = date(*map(int, start_day.split("-")))
+
+    # # Add B.1.177 strain from September 2020 and assume it's like b1351 (no
+    # # vaccine at this time in England)
+    # variants = []
+    # b1177 = cv.variant(
+    #     "b1351",
+    #     days=np.arange(sim.day("2020-08-10"), sim.day("2020-08-20")),
+    #     n_imports=3000,
+    # )
+    # b1177.p["rel_beta"] = 1.2
+    # b1177.p["rel_severe_prob"] = 0.4
+    # variants += [b1177]
+    first_day = (date(*map(int, "2020-08-10".split("-"))) - start_day).days
+    last_day = (date(*map(int, "2020-08-20".split("-"))) - start_day).days
+    b1351 = VariantSpec(
+        sequence=reference_sequences["Beta"],
+        variant={
+            **baseline_variant_params,
+            "rel_beta": 1.2,
+            "rel_severe_prob": 0.4,
+        },
+        label="Beta",
+        days=np.arange(first_day, last_day),
+        n_imports=60,
+    )
+
+    # # Add Alpha strain from October 2020
+    # b117 = cv.variant(
+    #     "b117",
+    #     days=np.arange(sim.day("2020-10-20"), sim.day("2020-10-30")),
+    #     n_imports=3000,
+    # )
+    # b117.p["rel_beta"] = 1.8
+    # b117.p["rel_severe_prob"] = 0.4
+    # variants += [b117]
+    first_day = (date(*map(int, "2020-10-20".split("-"))) - start_day).days
+    last_day = (date(*map(int, "2020-10-30".split("-"))) - start_day).days
+    b117 = VariantSpec(
+        sequence=reference_sequences["Alpha"],
+        variant={
+            **baseline_variant_params,
+            "rel_beta": 1.8,
+            "rel_severe_prob": 0.4,
+        },
+        label="Alpha",
+        days=np.arange(first_day, last_day),
+        n_imports=60,
+    )
+
+    # # Add Delta strain starting middle of April
+    # b16172 = cv.variant(
+    #     "b16172",
+    #     days=np.arange(sim.day("2021-04-15"), sim.day("2021-04-20")),
+    #     n_imports=4000,
+    # )
+    # b16172.p["rel_beta"] = 3.1
+    # b16172.p["rel_severe_prob"] = 0.2
+    # variants += [b16172]
+    first_day = (date(*map(int, "2021-04-15".split("-"))) - start_day).days
+    last_day = (date(*map(int, "2021-04-20".split("-"))) - start_day).days
+    b16172 = VariantSpec(
+        sequence=reference_sequences["Delta"],
+        variant={
+            **baseline_variant_params,
+            "rel_beta": 3.1,
+            "rel_severe_prob": 0.2,
+        },
+        label="Delta",
+        days=np.arange(first_day, last_day),
+        n_imports=80,
+    )
+
+    return [wildtype, b1351, b117, b16172]
---
+++ tests/test_cli/test_run_covaphastsim.py
@@ -0,0 +1,38 @@
+import os
+import subprocess
+import sys
+
+from pylib._cd_tmpdir_context import cd_tempdir_context
+
+assets = os.path.join(os.path.dirname(os.path.abspath(__file__)), "../assets")
+pwd = os.getcwd()
+
+
+@cd_tempdir_context()
+def test_run_covaphastsim_smoke():
+    subprocess.run(
+        [
+            sys.executable,
+            "-m",
+            "pylib.cli.run_covaphastsim",
+        ],
+        check=True,
+        env={"PYTHONPATH": f"{pwd}"},
+        input=f"""
+cfg_make_cv_sim_recipe: "make_cv_sim_vanilla"
+cfg_make_wt_specs_recipe: "make_wt_specs_single"
+cfg_num_mut_sites: 1
+cfg_p_wt_to_mut: 0.01
+cfg_p_seq_mut: 2.74e-6
+cfg_pop_size: 10000
+cfg_refseqs: "{assets}/alignedsequences.csv"
+cfg_suffix_mut: "'"
+cfg_suffix_wt: "+"
+replicate_num: 0
+trt_mutmx_active_strain_factor: 1.0
+trt_mutmx_rel_beta: 1.0
+trt_mutmx_withinhost_r: 1.0
+trt_name: "test"
+trt_seed: 1
+""".encode(),
+    )
