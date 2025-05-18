782363534a19565674d60d8d4770f660c250fd11
mmore500 <mmore500.login@gmail.com>
2025-05-18 18:00:15 +0000

    Deploying to binder-2025-04-11-dev-sequenceconfig.ipynb from @ mmore500/multilevel-selection-concept@2aeb9ff9c584df601d7c5d30e367de85f0f36b01 🚀

--- slurm_/2025-05-17-covasim-uk/2025-05-17-uk-run_compscreen.sh
+++ slurm_/2025-05-17-covasim-uk/2025-05-17-uk-run_compscreen.sh
@@ -19,7 +19,7 @@ echo "SOURCE_REVISION ${SOURCE_REVISION}"
 SOURCE_REMOTE_URL="$(git config --get remote.origin.url)"
 echo "SOURCE_REMOTE_URL ${SOURCE_REMOTE_URL}"
 
-SHA="af2e987daa8307fd615e926298cd701e6b8e285ad5f1385d48dd7c099335a8a5"
+SHA="3047c1f679e14058bde0e1858718e5c3481de124c6bd05bd5e12ddba3c5fd7f4"
 echo "SHA ${SHA}"
 REPO="docker://ghcr.io/mmore500/multilevel-selection-concept"
 echo "REPO ${REPO}"
@@ -316,7 +316,7 @@ cat > "${SBATCH_FILE}" << EOF
 #!/bin/bash
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
-#SBATCH --mem=16G
+#SBATCH --mem=64G
 #SBATCH --time=4:00:00
 #SBATCH --output="/mnt/home/%u/joblog/%j"
 #SBATCH --mail-user=mawni4ah2o@pomail.net
