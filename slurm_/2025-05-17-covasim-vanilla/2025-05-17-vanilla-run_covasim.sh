41daa49d6b2a6125b5619f5b67519314b360445f
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2025-05-17 20:26:20 -0400

    Configure n_imports_mx

--- slurm_/2025-05-17-covasim-vanilla/2025-05-17-vanilla-run_covasim.sh
+++ slurm_/2025-05-17-covasim-vanilla/2025-05-17-vanilla-run_covasim.sh
@@ -282,6 +282,7 @@ trt_mutmx_withinhost_r = {
 }[S]
 
 cfg = f"""
+cfg_n_imports_mx: 1000
 cfg_make_cv_sim_recipe: "make_cv_sim_vanilla"
 cfg_make_wt_specs_recipe: "make_wt_specs_single"
 cfg_num_mut_sites: 1
