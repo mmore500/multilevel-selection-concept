2aeb9ff9c584df601d7c5d30e367de85f0f36b01
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2025-05-18 13:46:10 -0400

    Bump container pin

--- slurm_/2025-05-17-covasim-multistrain/2025-05-17-multistrain-run_compscreen.sh
+++ slurm_/2025-05-17-covasim-multistrain/2025-05-17-multistrain-run_compscreen.sh
@@ -19,7 +19,7 @@ echo "SOURCE_REVISION ${SOURCE_REVISION}"
 SOURCE_REMOTE_URL="$(git config --get remote.origin.url)"
 echo "SOURCE_REMOTE_URL ${SOURCE_REMOTE_URL}"
 
-SHA="af2e987daa8307fd615e926298cd701e6b8e285ad5f1385d48dd7c099335a8a5"
+SHA="3047c1f679e14058bde0e1858718e5c3481de124c6bd05bd5e12ddba3c5fd7f4"
 echo "SHA ${SHA}"
 REPO="docker://ghcr.io/mmore500/multilevel-selection-concept"
 echo "REPO ${REPO}"
