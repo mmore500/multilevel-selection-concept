ed320bc5f8bc56d8d93703f626c96fd670b09b3c
mmore500 <mmore500.login@gmail.com>
2025-05-20 11:41:42 +0000

    Deploying to binder-2025-05-05-vanillasimplesingle-mut-screen.ipynb from @ mmore500/multilevel-selection-concept@27cfe437ae8fefa1c50e6e7bee728ea21899618f 🚀

--- slurm_/2025-05-19-covasim-dummy/2025-05-19-dummy-run_compscreen.sh
+++ slurm_/2025-05-19-covasim-dummy/2025-05-19-dummy-run_compscreen.sh
@@ -19,7 +19,7 @@ echo "SOURCE_REVISION ${SOURCE_REVISION}"
 SOURCE_REMOTE_URL="$(git config --get remote.origin.url)"
 echo "SOURCE_REMOTE_URL ${SOURCE_REMOTE_URL}"
 
-SHA="aa8cedbeaa38f83a57239bb7d0cf0275b98bca5a31f93096ef25ddbda2dbc52e"
+SHA="12d0ddc37ba360df419f81dc94b49b6e27fca2798371cd038fe87f85eba1ff11"
 echo "SHA ${SHA}"
 REPO="docker://ghcr.io/mmore500/multilevel-selection-concept"
 echo "REPO ${REPO}"
