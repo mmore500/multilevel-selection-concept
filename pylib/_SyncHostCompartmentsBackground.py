601a6b4864d85a87cfcf56fb690247a1509a0eca
mmore500 <mmore500.login@gmail.com>
2025-05-20 03:21:50 +0000

    Deploying to binder-2025-04-08-dev-sequencescreen-pessimal.ipynb from @ mmore500/multilevel-selection-concept@4bfc78eb37bd1daadcb93fa34104551da5009b4d 🚀

--- pylib/_SyncHostCompartmentsBackground.py
+++ pylib/_SyncHostCompartmentsBackground.py
@@ -2,6 +2,7 @@ import logging
 
 import covasim as cv
 import numpy as np
+from scipy import stats as scipy_stats
 
 from ._VariantFlavor import VariantFlavor
 
@@ -81,8 +82,8 @@ class SyncHostCompartmentsBackground:
             # zero out non-infectious/exposed compartments
             compartments[target, :, :] = 0.0
 
-            # init w/ covasim infectious variant
-            compartments[target, variant, :] = 1.0
+            for bg_site, variant in enumerate(variant):
+                compartments[target, variant, bg_site] = 1.0
 
         self._infection_log_pos = len(log)
 
@@ -101,9 +102,7 @@ class SyncHostCompartmentsBackground:
             )
             if not num_doublings > 0:
                 raise ValueError
-            p_per_doubling = 1.0 - np.power(
-                1.0 - variant_flavor.p_wt_to_mut, 1 / num_doublings
-            )
+            p_per_doubling = variant_flavor.p_wt_to_mut
 
             wt_growth_per_doubling = variant_flavor.withinhost_r_wt ** (
                 1 / num_doublings
@@ -115,6 +114,7 @@ class SyncHostCompartmentsBackground:
                 )
                 < 1e-6
             )
+            assert wt_growth_per_doubling >= 1
             for __ in range(num_doublings):
                 compartments[:, offset, :] *= wt_growth_per_doubling
                 compartments[:, offset + 1, :] *= wt_growth_per_doubling
@@ -148,6 +148,7 @@ class SyncHostCompartmentsBackground:
         assert np.all(divisor >= 1.0)
         assert np.isfinite(divisor).all()
         compartments /= divisor
+        assert np.isfinite(compartments).all()
 
         ## sync host compartments to covasim "infectious variant"
         #######################################################################
@@ -172,6 +173,10 @@ class SyncHostCompartmentsBackground:
             np.nan,
         )
         assert len(sampled_strains) == compartments_.shape[0]
+        assert sampled_strains.shape == (
+            compartments_.shape[0],
+            compartments_.shape[2],
+        )
 
         self._last_sampled_strains = np.where(
             ~np.isnan(sampled_strains),
@@ -183,6 +188,11 @@ class SyncHostCompartmentsBackground:
         self._infectious_variants_log.append(sampled_strains)
 
         ## sample variants of record
+        upper_bound = scipy_stats.binom.isf(
+            1e-9,
+            compartments_.shape[2],
+            p_per_doubling * num_doublings * 8,
+        )
         for who in np.flatnonzero(self._infection_days_elapsed >= 8):
             entry = self._infection_log_entries[who]
             assert entry is not None
@@ -190,19 +200,22 @@ class SyncHostCompartmentsBackground:
             variant = self._last_sampled_strains[who].astype(int)
             assert (np.clip(variant, 1, num_variants - 1) == variant).all()
             sampled_strain = "".join(["'", "+"][v % 2] for v in variant)
+
+            num_changes = sum(
+                x != y
+                for x, y in zip(entry["sequence_background"], sampled_strain)
+            )
             assert (
-                sum(
-                    x != y
-                    for x, y in zip(
-                        entry["sequence_background"], sampled_strain
-                    )
-                )
-                < 10
+                num_changes <= upper_bound
             ), f"""
+{num_changes=}
 {variant=}
 {who=}
 {entry=}
-{sampled_strain=}"
+{sampled_strain=}
+{upper_bound=}
+{p_per_doubling=}
+{compartments_.shape[2]=}
 """
             entry["sequence_background"] = sampled_strain
             self._infection_log_entries[who] = None
