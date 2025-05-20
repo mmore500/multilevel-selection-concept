791f4ab0b268569f8d4bc5f668b40fb89d97b2c9
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2025-05-19 23:30:12 -0400

    Bump container pins

--- slurm_/2025-05-17-covasim-uk/2025-05-17-uk-run_compscreen.sh
+++ slurm_/2025-05-17-covasim-uk/2025-05-17-uk-run_compscreen.sh
@@ -19,7 +19,7 @@ echo "SOURCE_REVISION ${SOURCE_REVISION}"
 SOURCE_REMOTE_URL="$(git config --get remote.origin.url)"
 echo "SOURCE_REMOTE_URL ${SOURCE_REMOTE_URL}"
 
-SHA="aae80bc17e05b7f75e9a5efc5497636f8d0fc64226228387f0712b6adaa002f9"
+SHA="aa8cedbeaa38f83a57239bb7d0cf0275b98bca5a31f93096ef25ddbda2dbc52e"
 echo "SHA ${SHA}"
 REPO="docker://ghcr.io/mmore500/multilevel-selection-concept"
 echo "REPO ${REPO}"
