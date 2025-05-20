dbf6778267d6f10674f70158b1bf2698e109696f
mmore500 <mmore500.login@gmail.com>
2025-05-20 03:13:20 +0000

    Deploying to binder-2025-05-19-compscreen-uk-summary.ipynb from @ mmore500/multilevel-selection-concept@4bfc78eb37bd1daadcb93fa34104551da5009b4d 🚀

--- slurm_/2025-05-17-covasim-uk/2025-05-17-uk-run_covasim.sh
+++ slurm_/2025-05-17-covasim-uk/2025-05-17-uk-run_covasim.sh
@@ -286,7 +286,7 @@ cfg_n_imports_mx: 1000
 cfg_make_cv_sim_recipe: "make_cv_sim_uk"
 cfg_make_wt_specs_recipe: "make_wt_specs_single"
 cfg_num_mut_sites: 1
-cfg_p_wt_to_mut: {1e-5:.60f}
+cfg_p_wt_to_mut: {2e-6:.60f}  # https://doi.org/10.1038/s41579-023-00878-2
 cfg_pop_size: {200_000}
 cfg_refseqs: "https://osf.io/hp25c/download"
 cfg_suffix_mut: "'"
