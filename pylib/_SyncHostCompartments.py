37f2f501c7bd3ade4b7b6b8fb2836d0dbd9f5603
mmore500 <mmore500.login@gmail.com>
2025-05-20 00:34:01 +0000

    Deploying to binder-2025-04-08-dev-sequencescreen-beneficialinhost.ipynb from @ mmore500/multilevel-selection-concept@511fdba5ed9b9ecb858a5859be4ffa0ed64043a4 🚀

--- pylib/_SyncHostCompartments.py
+++ pylib/_SyncHostCompartments.py
@@ -1,3 +1,5 @@
+import logging
+
 import covasim as cv
 import numpy as np
 
@@ -46,6 +48,9 @@ class SyncHostCompartments:
 
         ## sync covasim to host compartments
         #######################################################################
+        logging.info(
+            f"num_infections focal = {len(log[self._infection_log_pos :])=}",
+        )
         for entry in log[self._infection_log_pos :]:
             target, variant = (entry["target"], entry["variant"])
             self._infection_log_entries[target] = entry
