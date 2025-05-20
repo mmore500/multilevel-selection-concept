7c3fd032a8f39344e60bfd25760bcd6a65d0c634
mmore500 <mmore500.login@gmail.com>
2025-05-20 11:58:20 +0000

    Deploying to binder-2025-04-30-profile-mut-freqs.ipynb from @ mmore500/multilevel-selection-concept@27cfe437ae8fefa1c50e6e7bee728ea21899618f 🚀

--- slurm_/2025-05-18-covasim-vanilla-big/2025-05-18-vanilla-big-run_compscreen.sh
+++ slurm_/2025-05-18-covasim-vanilla-big/2025-05-18-vanilla-big-run_compscreen.sh
@@ -250,7 +250,7 @@ import pandas as pd
 from retry import retry
 
 
-refphylos = "https://osf.io/yspd5/download"
+refphylos = "https://osf.io/mr7kv/download"
 
 read_parquet = retry(tries=5, logger=logging.getLogger(__name__))(pd.read_parquet)
 uuids = sorted(
