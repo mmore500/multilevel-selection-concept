42b1f524c8ad5828b95b3b8c28d488aa950ebc59
mmore500 <mmore500.login@gmail.com>
2025-05-20 03:14:46 +0000

    Deploying to binder-2025-05-14-vanilla-mut-screen.ipynb from @ mmore500/multilevel-selection-concept@4bfc78eb37bd1daadcb93fa34104551da5009b4d 🚀

--- slurm_/2025-05-18-covasim-vanilla-big-1-3x/2025-05-18-vanilla-big-1-3x-run_covasim.sh
+++ slurm_/2025-05-18-covasim-vanilla-big-1-3x/2025-05-18-vanilla-big-1-3x-run_covasim.sh
@@ -281,7 +281,7 @@ cfg_n_imports_mx: 1000
 cfg_make_cv_sim_recipe: "make_cv_sim_vanilla"
 cfg_make_wt_specs_recipe: "make_wt_specs_single"
 cfg_num_mut_sites: 1
-cfg_p_wt_to_mut: {1e-5:.60f}
+cfg_p_wt_to_mut: {2e-6:.60f}  # https://doi.org/10.1038/s41579-023-00878-2
 cfg_pop_size: {1_200_000}
 cfg_refseqs: "https://osf.io/s9xhr/download"  # homogenized seqs for testing
 cfg_suffix_mut: "'"
