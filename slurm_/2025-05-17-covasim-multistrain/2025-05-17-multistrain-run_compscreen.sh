853bd3efb639d8880cfc889dd8ef9303d78722ea
mmore500 <mmore500.login@gmail.com>
2025-05-20 14:29:27 +0000

    Deploying to binder-2025-04-08-dev-sequencescreen.ipynb from @ mmore500/multilevel-selection-concept@ac3f74e97ec3a1ae8d85246aa917d4cc5d696fc0 🚀

--- slurm_/2025-05-17-covasim-multistrain/2025-05-17-multistrain-run_compscreen.sh
+++ slurm_/2025-05-17-covasim-multistrain/2025-05-17-multistrain-run_compscreen.sh
@@ -263,7 +263,6 @@ replicates = it.product(
     [1_000_000],
 )
 replicates = [*replicates]
-assert len(replicates) == 35, f"expected 35 replicates, got {len(replicates)}"
 
 assigned_uuid, hsurf_bits, ndownsamp = next(
     it.islice(replicates, \${SLURM_ARRAY_TASK_ID:-0}, None),
